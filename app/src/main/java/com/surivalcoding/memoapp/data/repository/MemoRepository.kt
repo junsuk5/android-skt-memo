aa6298a088d92b719326da6f340132e4d02cceb1
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-04-05 11:23:01 +0900

    fix : MemoRepository 수정

--- app/src/main/java/com/surivalcoding/memoapp/data/repository/MemoRepository.kt
+++ app/src/main/java/com/surivalcoding/memoapp/data/repository/MemoRepository.kt
@@ -1,4 +1,10 @@
 package com.surivalcoding.memoapp.data.repository
 
+import com.surivalcoding.memoapp.data.model.Memo
+
 interface MemoRepository {
+    suspend fun insert(memo: Memo)
+    suspend fun update(memo: Memo)
+    suspend fun delete(memo: Memo)
+    suspend fun findAll(): List<Memo>
 }
